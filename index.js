b88d4445d80d20487444f8461949b7d7490d74f3
Oscar-henri STEFANINI <oscar.stefanini@epitech.eu>
2018-10-02 13:51:07 -0400

    index.js should be reserved for the module

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const files_sync_loader = require("./files-sync-loader.js")
+const glob = require("glob")
+const path = require('path')
 
-const files = new files_sync_loader("./jsons/*.json", '.json');
+class files_sync_loader {
+    constructor(searching_path, ext) {
+	
+	const options = {
+	    'nosort': true, // dont need to sort files
+	    'strict': true, // mark an error if any readdir's problems happen
+	    'nodir': true // we only need to load ext files
+	}
+	
+	const files = glob.sync(searching_path, options)
+	this._json = {};
+	for (let i = 0; i < files.length; ++i)
+	{
+	    this._json[path.basename(files[i], ext)] = require(files[i]);
+	}
+    }
 
-console.log(files.json);
+    get json() {
+	return (this._json);
+    }
+}
+
+module.exports = files_sync_loader;
